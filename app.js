2a45426bb2c910b1b1f8c72a5b974774b8e4b702
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-01-16 14:58:28 +1100

    Working with express routing.

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 const express = require("express");
 const app = express();
 
-app.use((req, res) => {
-  console.log("we got req");
-  //   res.send("hello we got yor request!");
-  res.send("<h1>My webpage</h1>");
+// app.use((req, res) => {
+//   console.log("we got req");
+//   // res.send("hello we got yor request!");
+//   res.send("<h1>My webpage</h1>");
+// });
+
+// /cats
+app.get("/cats", (req, res) => {
+  res.send("This is cats router");
+});
+
+app.post("/cats", (req, res) => {
+  res.send("This is a post request to /cats");
+});
+
+// /dogs
+app.get("/dogs", (req, res) => {
+  res.send("<h2>Woof Woof</h2>");
+});
+
+// '/'
+app.get("/", (req, res) => {
+  res.send("<h1>This is the Home page.</h1>");
+});
+
+app.get("*", (req, res) => {
+  res.send(`I don't know this path`);
 });
 
 app.listen(8080, () => {
